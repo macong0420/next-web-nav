9da3dec68f30a1af5d926810b4bc427c9edca3b2
小马同学 <macong0420@126.com>
2024-11-15 11:42:27 +0800

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -28,6 +28,53 @@ type NavData = {
 
 export const NavData: NavData[] = [
   {
+    title: "工作相关",
+    items: [
+      {
+        icon: "https://raw.githubusercontent.com/macong0420/Image/main/202411151135283.png",
+        title: "gerrit",
+        desc: "代码提交",
+        link: "http://gerrit.lianjia.com/#/dashboard/self",
+      },
+      {
+        icon: "https://raw.githubusercontent.com/macong0420/Image/main/202411151134074.png",
+        title: "ones",
+        desc: "ones",
+        link: "https://ones.ke.com",
+      },
+      {
+        icon: "https://raw.githubusercontent.com/macong0420/Image/main/202411151136035.png",
+        title: "贝拉",
+        desc: "beila",
+        link: "https://bella.ke.com/#/mainArch/%5Bobject%20Object%5D",
+      },
+      {
+        icon: "https://raw.githubusercontent.com/macong0420/Image/main/202411151137484.png",
+        title: "海神",
+        desc: "海神",
+        link: "http://poseidon.bugly.ke.com/page/homePage?app_id=1&app_identifier=com.lianjia.beike&platform=ios",
+      },
+      {
+        icon: "https://raw.githubusercontent.com/macong0420/Image/main/202411151138353.png",
+        title: "会议室",
+        desc: "会议室",
+        link: "https://new-meeting.ke.com/roomBoard",
+      },
+      {
+        icon: "https://raw.githubusercontent.com/macong0420/Image/main/202411151138353.png",
+        title: "珊瑚代客",
+        desc: "珊瑚代客",
+        link: "https://s.a.ke.com/page/home",
+      },
+      {
+        icon: "https://raw.githubusercontent.com/macong0420/Image/main/202411151138353.png",
+        title: "周会",
+        desc: "周会",
+        link: "https://wiki.lianjia.com/pages/viewpage.action?pageId=1338487367",
+      },
+    ],
+  },
+  {
     title: "常用工具",
     items: [
       {
@@ -177,390 +224,6 @@ export const NavData: NavData[] = [
     ],
   },
   {
-    title: "Vue",
-    items: [
-      {
-        icon: "https://cn.vuejs.org/logo.svg",
-        title: "Vue 3",
-        desc: "渐进式 JavaScript 框架",
-        link: "https://cn.vuejs.org",
-      },
-      {
-        icon: "https://cn.vuejs.org/logo.svg",
-        title: "Vue 2",
-        desc: "渐进式 JavaScript 框架",
-        link: "https://v2.cn.vuejs.org",
-      },
-      {
-        icon: "https://cn.vuejs.org/logo.svg",
-        title: "Vue Router",
-        desc: "Vue.js 的官方路由\n为 Vue.js 提供富有表现力、可配置的、方便的路由",
-        link: "https://router.vuejs.org/zh",
-      },
-      {
-        icon: "https://pinia.vuejs.org/logo.svg",
-        title: "Pinia",
-        desc: "符合直觉的 Vue.js 状态管理库",
-        link: "https://pinia.vuejs.org/zh",
-      },
-      {
-        icon: "https://nuxt.com/icon.png",
-        title: "Nuxt.js",
-        desc: "一个基于 Vue.js 的通用应用框架",
-        link: "https://nuxt.com",
-      },
-      {
-        icon: "https://vueuse.org/favicon.svg",
-        title: "VueUse",
-        desc: "Vue Composition API 的常用工具集",
-        link: "https://vueuse.org",
-      },
-      {
-        icon: "https://vitest.dev/favicon.ico",
-        title: "Vitest",
-        desc: "一个 Vite 原生单元测试框架。它很快！",
-        link: "https://cn.vitest.dev/",
-      },
-      {
-        icon: "https://element-plus.org/images/element-plus-logo-small.svg",
-        title: "Element Plus",
-        desc: "基于 Vue 3，面向设计师和开发者的组件库",
-        link: "https://element-plus.org",
-      },
-      {
-        icon: "https://www.antdv.com/assets/logo.1ef800a8.svg",
-        title: "Ant Design Vue",
-        desc: "Ant Design 的 Vue 实现，开发和服务于企业级后台产品",
-        link: "https://antdv.com",
-      },
-      {
-        icon: "https://fastly.jsdelivr.net/npm/@vant/assets/logo.png",
-        title: "Vant",
-        desc: "轻量、可定制的移动端 Vue 组件库",
-        link: "https://vant-ui.github.io/vant",
-      },
-      {
-        icon: "https://webapp.didistatic.com/static/webapp/shield/Cube-UI_logo.ico",
-        title: "Cube UI",
-        desc: "基于 Vue.js 实现的精致移动端组件库",
-        link: "https://didi.github.io/cube-ui",
-      },
-      {
-        icon: "https://img14.360buyimg.com/imagetools/jfs/t1/167902/2/8762/791358/603742d7E9b4275e3/e09d8f9a8bf4c0ef.png",
-        title: "NutUI",
-        desc: "京东风格的轻量级移动端组件库",
-        link: "https://nutui.jd.com",
-      },
-    ],
-  },
-  {
-    title: "JavaScript",
-    items: [
-      {
-        icon: "https://svelte.dev/svelte-logo-horizontal.svg",
-        title: "Svelte",
-        desc: "将声明性组件转换为精准高效更新 DOM 的 JavaScript 代码",
-        link: "https://svelte.dev",
-      },
-      {
-        icon: "/icons/jquery.svg",
-        title: "jQuery API 中文文档",
-        desc: "一个兼容多浏览器的 JavaScript 框架",
-        link: "https://jquery.cuishifeng.cn",
-      },
-      {
-        title: "ECharts",
-        desc: "一个基于 JavaScript 的开源可视化图表库",
-        icon: "https://echarts.apache.org/zh/images/favicon.png",
-        link: "https://echarts.apache.org/",
-      },
-      {
-        title: "AntV",
-        desc: "蚂蚁集团全新一代数据可视化解决方案,让数据栩栩如生",
-        icon: "/icons/antv.png",
-        link: "https://antv.vision/",
-      },
-    ],
-  },
-  {
-    title: "CSS",
-    items: [
-      {
-        icon: "https://postcss.org/assets/logo-3e39b0aa.svg",
-        title: "PostCSS",
-        desc: "一个用 JavaScript 转换 CSS 的工具",
-        link: "https://postcss.org",
-      },
-      {
-        icon: "https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg",
-        title: "Sass",
-        desc: "一个成熟，稳定，功能强大的专业级 CSS 扩展语言",
-        link: "https://sass-lang.com",
-      },
-      {
-        icon: "https://www.tailwindcss.cn/apple-touch-icon.png",
-        title: "TailwindCSS 中文网",
-        desc: "一个功能类优先的 CSS 框架",
-        link: "https://www.tailwindcss.cn",
-      },
-      {
-        title: "WindiCSS",
-        desc: "Windi CSS 是下一代工具优先的 CSS 框架",
-        icon: "https://windicss.org/assets/logo.svg",
-        link: "https://windicss.org",
-      },
-      {
-        title: "Twind",
-        desc: "现存最小、最快、功能最齐全的完整 Tailwind-in-JS 解决方案",
-        icon: "/icons/twind.svg",
-        link: "https://github.com/tw-in-js/twind",
-      },
-      {
-        title: "UnoCSS",
-        desc: "即时按需原子 CSS 引擎",
-        icon: "https://uno.antfu.me//favicon.svg",
-        link: "https://uno.antfu.me/",
-      },
-      {
-        title: "Bootstrap",
-        desc: "Bootstrap 是全球最受欢迎的前端开源工具库，它支持 Sass 变量和 mixin、响应式栅格系统、自带大量组件和众多强大的 JavaScript 插件。基于 Bootstrap 提供的强大功能，能够让你快速设计并定制你的网站",
-        icon: "https://img.kuizuo.cn/20210907055816.png",
-        link: "https://v5.bootcss.com/",
-      },
-      {
-        title: "w3schools Css 教程",
-        desc: "w3schools 从基础到高级的CSS教程",
-        icon: "https://www.w3schools.com/favicon.ico",
-        link: "https://www.w3schools.com/css",
-      },
-      {
-        title: "CSS-Inspiration",
-        desc: "CSS灵感",
-        icon: "/icons/css-inspiration.png",
-        link: "https://csscoco.com/inspiration",
-      },
-      {
-        title: "CSS常用样式",
-        desc: "CSS常用样式",
-        icon: "https://tse1-mm.cn.bing.net/th?id=OIP-C.EgSPriuEnAtlIWJV8R_E1QHaGs&w=107&h=100&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
-        link: "https://github.com/QiShaoXuan/css_tricks",
-      },
-      {
-        title: "CSSFX",
-        desc: "一个精心制作的集合设计的重点是流动性，简单性和易用性。使用最小标记的 CSS 支持",
-        icon: "/icons/cssfx.png",
-        link: "https://cssfx.netlify.app/",
-      },
-      {
-        title: "NES.css",
-        desc: "一个像素风格的CSS框架",
-        icon: "https://nostalgic-css.github.io/NES.css/favicon.png",
-        link: "https://nostalgic-css.github.io/NES.css/",
-      },
-      {
-        title: "clay.css",
-        desc: "claymorphism 泥陶态风格CSS",
-        icon: "https://codeadrian.github.io/clay.css/apple-touch-icon.png",
-        link: "https://codeadrian.github.io/clay.css/",
-      },
-      {
-        title: "loading.io",
-        desc: "Animation Made Easy",
-        icon: "/icons/loading.ico",
-        link: "https://loading.io/",
-      },
-      {
-        title: "神奇UI样式",
-        desc: "我们赋予任何人创建、分享和使用用 CSS 和 HTML 制作的漂亮自定义元素的权力。",
-        icon: "/icons/uiverse.png",
-        link: "https://uiverse.io",
-      },
-      {
-        title: "HYPE4",
-        desc: "透明玻璃态生成器",
-        icon: "https://hype4.academy/_next/static/media/logorwd@2x.b40bc92c.png",
-        link: "https://hype4.academy/tools/glassmorphism-generator",
-      },
-      {
-        title: "Omatsuri",
-        desc: "收集不同的发电机，让您的生活更轻松。",
-        icon: "https://omatsuri.app/assets/favicon.ico",
-        link: "https://omatsuri.app",
-      },
-      {
-        title: "smooth shadow",
-        desc: "快速轻松地实现基于 CSS 阴影的绝佳工具。您只需要指定一些阴影设置，代码就在您的路上。",
-        icon: "https://shadows.brumm.af/favicon.svg",
-        link: "https://shadows.brumm.af/",
-      },
-      {
-        title: "FANCY-BORDER-RADIUS",
-        desc: "花式边界半径,有助于创建 CSS 花式边框。",
-        icon: "https://9elements.github.io/fancy-border-radius/favicon-32x32.png",
-        link: "https://9elements.github.io/fancy-border-radius/",
-      },
-      {
-        title: "Coolors",
-        desc: "创建调色板",
-        icon: "/icons/coolors.png",
-        link: "https://coolors.co/",
-      },
-    ],
-  },
-  {
-    title: "Node",
-    items: [
-      {
-        icon: "https://deno.com/logo.svg",
-        title: "deno",
-        desc: "Deno is the open-source JavaScript runtime for the modern web.",
-        link: "https://deno.com/",
-      },
-      {
-        icon: "https://nodejs.org/static/images/favicons/favicon.png",
-        title: "Node.js",
-        desc: "Node.js 是一个基于 Chrome V8 引擎的 JavaScript 运行环境",
-        link: "https://nodejs.org/zh-cn",
-      },
-      {
-        icon: "https://expressjs.com/images/favicon.png",
-        title: "Express",
-        desc: "基于 Node.js 平台，快速、开放、极简的 Web 开发框架",
-        link: "https://expressjs.com",
-      },
-      {
-        icon: "https://nodejs.org/static/images/favicons/favicon.png",
-        title: "Koa",
-        desc: "基于 Node.js 平台的下一代 web 开发框架",
-        link: "https://koajs.com",
-      },
-      {
-        icon: "https://www.eggjs.org/favicon.png",
-        title: "Egg",
-        desc: "为企业级框架和应用而生",
-        link: "https://www.eggjs.org/zh-CN",
-      },
-      {
-        icon: "https://d33wubrfki0l68.cloudfront.net/e937e774cbbe23635999615ad5d7732decad182a/26072/logo-small.ede75a6b.svg",
-        title: "Nest.js 中文文档",
-        desc: "用于构建高效且可伸缩的服务端应用程序的渐进式 Node.js 框架",
-        link: "https://docs.nestjs.cn",
-      },
-      {
-        title: "TypeORM",
-        desc: "TypeORM 是一个 ORM 框架，它可以运行在 NodeJS、Browser、Cordova、PhoneGap、Ionic、React Native、Expo 和 Electron 平台上，可以与 TypeScript 和 JavaScript (ES5,ES6,ES7,ES8)一起使用",
-        icon: "/icons/typeorm.ico",
-        link: "https://typeorm.bootcss.com",
-      },
-      {
-        title: "Axios",
-        desc: "Axios 是一个基于 promise 的网络请求库，可以用于浏览器和 node.js",
-        icon: "/icons/axios.ico",
-        link: "https://axios-http.cn/",
-      },
-      {
-        title: "NPM",
-        desc: "NPM是世界上最大的包管理器",
-        icon: "https://static.npmjs.com/58a19602036db1daee0d7863c94673a4.png",
-        link: "https://www.npmjs.com",
-      },
-      {
-        title: "Yarn",
-        desc: "Yarn 是一个软件包管理器，还可以作为项目管理工具。无论你是小型项目还是大型单体仓库（monorepos），无论是业余爱好者还是企业用户，Yarn 都能满足你的需求",
-        icon: "https://www.yarnpkg.cn/favicon-32x32.png",
-        link: "https://www.yarnpkg.cn",
-      },
-      {
-        title: "Pnpm",
-        desc: "速度快、节省磁盘空间的软件包管理器",
-        icon: "https://www.pnpm.cn/img/favicon.png",
-        link: "https://pnpm.io",
-      },
-    ],
-  },
-  {
-    title: "构建工具",
-    items: [
-      {
-        icon: "https://www.webpackjs.com/icon_180x180.png",
-        title: "Webpack 中文网",
-        desc: "一个用于现代 JavaScript 应用程序的静态模块打包工具",
-        link: "https://www.webpackjs.com",
-      },
-      {
-        icon: "https://cn.vitejs.dev/logo.svg",
-        title: "Vite 中文文档",
-        desc: "下一代前端工具链",
-        link: "https://cn.vitejs.dev",
-      },
-      {
-        icon: "https://www.rollupjs.com/img/favicon.png",
-        title: "Rollup",
-        desc: "Rollup 是一个 JavaScript 模块打包器",
-        link: "https://www.rollupjs.com",
-      },
-      {
-        icon: "https://turbo.build/images/favicon-dark/apple-touch-icon.png",
-        title: "Turbo",
-        desc: "Turbo is an incremental bundler and build system optimized for JavaScript and TypeScript, written in Rust",
-        link: "https://turbo.build",
-      },
-      {
-        icon: "https://www.babeljs.cn/img/favicon.png",
-        title: "Babel",
-        desc: "Babel 是一个 JavaScript 编译器",
-        link: "https://www.babeljs.cn",
-      },
-      {
-        icon: "https://esbuild.github.io/favicon.svg",
-        title: "esbuild",
-        desc: "An extremely fast bundler for the web",
-        link: "https://esbuild.github.io",
-      },
-      {
-        icon: "https://swc.rs/favicon/apple-touch-icon.png",
-        title: "SWC",
-        desc: "Rust-based platform for the Web",
-        link: "https://swc.rs",
-      },
-    ],
-  },
-  {
-    title: "前端文档",
-    items: [
-      {
-        icon: "https://developer.mozilla.org/apple-touch-icon.6803c6f0.png",
-        title: "MDN | Web 开发者指南",
-        desc: "Mozilla 的开发者平台，提供了大量关于 HTML、CSS 和 JavaScript 的详细文档以及广泛的 Web API 参考资料",
-        link: "https://developer.mozilla.org/zh-CN",
-      },
-      {
-        icon: "https://static.runoob.com/images/favicon.ico",
-        title: "菜鸟教程",
-        desc: "学的不仅是技术，更是梦想！",
-        link: "https://www.runoob.com",
-      },
-      {
-        icon: "/icons/es6.png",
-        title: "ES6 入门教程",
-        desc: "《ECMAScript 6 入门教程》是一本开源的 JavaScript 语言教程，全面介绍 ECMAScript 6 新引入的语法特性",
-        link: "https://es6.ruanyifeng.com/",
-      },
-      {
-        icon: "/icons/typescript.png",
-        title: "深入理解 TypeScript",
-        desc: "《TypeScript Deep Dive》 是一本很好的开源书，从基础到深入，很全面的阐述了 TypeScript 的各种魔法",
-        link: "https://jkchao.github.io/typescript-book-chinese/",
-      },
-      {
-        icon: "/icons/rust.svg",
-        title: "Rust语言圣经",
-        desc: "一份高质量 Rust 教程",
-        link: "https://course.rs",
-      },
-    ],
-  },
-  {
     title: "UI设计",
     items: [
       {
@@ -692,53 +355,6 @@ export const NavData: NavData[] = [
         desc: "Taro 是一个开放式跨端跨框架解决方案，支持使用 React/Vue/Nerv 等框架来开发 微信 / 京东 / 百度 / 支付宝 / 字节跳动 / QQ / 飞书 小程序 / H5 / RN 等应用",
         icon: "/icons/taro.png",
         link: "https://taro.jd.com",
-      },
-    ],
-  },
-  {
-    title: "站点生成",
-    items: [
-      {
-        title: "VitePress",
-        desc: "Vue 驱动并使用Vite构建的静态网站生成器",
-        icon: "https://vuepress.vuejs.org/hero.png",
-        link: "https://vitepress.vuejs.org",
-      },
-      {
-        title: "VuePress",
-        desc: "Vue 驱动的静态网站生成器",
-        icon: "https://vuepress.vuejs.org/hero.png",
-        link: "https://vuepress.vuejs.org",
-      },
-      {
-        title: "Docusaurus",
-        desc: "快速构建以内容为核心的最佳网站",
-        icon: "/icons/docusaurus.svg",
-        link: "https://docusaurus.io",
-      },
-      {
-        title: "Hexo",
-        desc: "快速、简洁且高效的博客框架",
-        icon: "https://hexo.io/favicon.ico",
-        link: "https://hexo.io",
-      },
-      {
-        title: "GitBook",
-        desc: "GitBook帮助您为用户发布漂亮的文档，并集中您的团队的知识进行高级协作",
-        icon: "https://assets-global.website-files.com/600ead1452cf056d0e52dbed/6246d2036225eac4d74cff27_Favicon_Blue.png",
-        link: "https://www.gitbook.com/",
-      },
-      {
-        title: "Docsify",
-        desc: "docsify 可以快速帮你生成文档网站",
-        icon: "https://docsify.js.org/_media/icon.svg",
-        link: "https://docsify.js.org",
-      },
-      {
-        title: "WordPress",
-        desc: "WordPress是一款能让您建立出色网站、博客或应用程序的开源软件",
-        icon: "https://s.w.org/images/wmark.png",
-        link: "https://cn.wordpress.org/",
       },
     ],
   },
